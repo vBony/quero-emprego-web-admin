df9e81d500bc949afbf1c250c13850efb2b6e9e3
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-06-23 18:53:51 -0300

    retirando header de ambiente de testes

--- app/core/controllerHelper.php
+++ app/core/controllerHelper.php
@@ -3,27 +3,11 @@ class controllerHelper{
     public function loadView($viewName, $viewData = array(), $show_header = true){
         extract($viewData);
 
-        if($_ENV['ENVIROMENT'] == 'development'){
-            $headerHomolog = file_get_contents($_ENV['BASE_URL'] . 'app/views/homolog-header.php');
-
-            if($show_header == true){
-                echo $headerHomolog;
-            }
-        }
-
         require 'app/views/'.$viewName.'.php';
     }
 
     public function loadTemplate($viewName, $viewData = array(), $show_header = true){
         extract ($viewData);
-
-        if($_ENV['ENVIROMENT'] == 'development'){
-            $headerHomolog = file_get_contents($_ENV['BASE_URL'] . 'app/views/homolog-header.php');
-
-            if($show_header == true){
-                echo $headerHomolog;
-            }
-        }
 
         require 'app/views/template.php';
     }
