d9ff8fee26cd497f2a3ad03b0243c32062ac7139
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-07-01 23:10:17 -0300

    listagem relatorio de acesso

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -5,11 +5,17 @@ class homeController extends controllerHelper{
         $data = array();
 
         $UserAdmin = new UserAdmin();
+        $RelatorioAcessos = new RelatorioAcessos();
+
         $user = $UserAdmin->buscar($_SESSION['token']['id']);
+        $relatorio = $RelatorioAcessos->buscar();
+        $countAcessos = $RelatorioAcessos->getCount();
 
         $data['css'] = 'home.css';
         $data['js'] = 'home.js';
         $data['user'] = $user;
+        $data['relatorio_acessos'] = $relatorio;
+        $data['count_acessos'] = $countAcessos['contagem'];
         $data['title'] = "📊 Dashboard - Quero Emprego Admin";
         
         $this->loadTemplate('home', $data);
